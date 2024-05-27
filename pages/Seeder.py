7cd0e2730db49d6884aa420ed168a436c86ca77f
NickBak08 <69905514+NickBak08@users.noreply.github.com>
2024-05-27 13:47:49 +0200

    Add bridge making to GUI

--- pages/Seeder.py
+++ pages/Seeder.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from ComputerVision.field_prediction import predict_field, predict_json, filter_json 
+from ComputerVision.field_prediction import predict_field, predict_json, filter_json, make_bridge
 from PathPlanning.pathplanning import pathplanning,load_data
 from Simulation.MPC_pathtracking import track_path
 from Simulation.PID_pathtracking import path_tracking
@@ -29,8 +29,11 @@ if 'show_field' not in st.session_state:
 if 'clicked_simulation' not in st.session_state:
     st.session_state.clicked_simulation = False
 
-if 'select_pixel' not in st.session_state:
-    st.session_state.select_pixel = False
+if 'select_pixel_scale' not in st.session_state:
+    st.session_state.select_pixel_scale = False
+
+if 'select_pixel_bridge' not in st.session_state:
+    st.session_state.select_pixel_bridge = False
 
 # When uploading a new field the GUI will be reset by settings all session_states to False
 # These functions need to be defined above there GUI component. 
@@ -40,7 +43,9 @@ def reset_gui():
     st.session_state.clicked_path = False
     st.session_state.show_field = False
     st.session_state.clicked_simulation = False
-    st.session_state.select_pixel = False
+    st.session_state.select_pixel_scale = False
+    st.session_state.select_pixel_bridge = False
+
 # Creating the title for uploading images
 st.title("Upload image field")
 
@@ -69,25 +74,49 @@ st.button("Detect fields",on_click=predict_button)
 # Title for setting the scaling factor
 st.title("Input size field")
 
-def choose_pixel_image():
+def choose_pixel_image_scale():
     '''Callback function for showing the pixels that are chosen'''
     pil_image = Image.open(uploaded_file).convert('RGB')
     open_cv_image = np.array(pil_image)
     print(open_cv_image.shape)
-    open_cv_image = cv2.circle(open_cv_image, (st.session_state['x_pixel1'],st.session_state['y_pixel1']), 5, color=(0, 0, 255), thickness=-1)
-    open_cv_image = cv2.circle(open_cv_image, (st.session_state['x_pixel2'],st.session_state['y_pixel2']), 5, color=(0, 0, 255), thickness=-1)
-    cv2.imwrite("filter/select_pixels.png",open_cv_image)
-    st.session_state.select_pixel = True
+    open_cv_image = cv2.circle(open_cv_image, (st.session_state['x_pixel1_scale'],st.session_state['y_pixel1_scale']), 5, color=(0, 0, 255), thickness=-1)
+    open_cv_image = cv2.circle(open_cv_image, (st.session_state['x_pixel2_scale'],st.session_state['y_pixel2_scale']), 5, color=(0, 0, 255), thickness=-1)
+    cv2.imwrite("filter/select_pixel_scales.png",open_cv_image)
+    st.session_state.select_pixel_scale = True
 
-if st.session_state.select_pixel:
-    st.image("./filter/select_pixels.png")
+if st.session_state.select_pixel_scale:
+    st.image("./filter/select_pixel_scales.png")
 
-x_pixel1 = st.number_input("X pixel 1",min_value=0,value=1,on_change=choose_pixel_image,key="x_pixel1")
-y_pixel1 = st.number_input("Y pixel 1",min_value=0,value=1,on_change=choose_pixel_image,key="y_pixel1")
-x_pixel2 = st.number_input("X pixel 2",min_value=0,value=640,on_change=choose_pixel_image,key="x_pixel2")
-y_pixel2 = st.number_input("Y pixel 2",min_value=0,value=640,on_change=choose_pixel_image,key="y_pixel2")
+x_pixel1_scale = st.number_input("X pixel 1",min_value=0,value=1,on_change=choose_pixel_image_scale,key="x_pixel1_scale")
+y_pixel1_scale = st.number_input("Y pixel 1",min_value=0,value=1,on_change=choose_pixel_image_scale,key="y_pixel1_scale")
+x_pixel2_scale = st.number_input("X pixel 2",min_value=0,value=640,on_change=choose_pixel_image_scale,key="x_pixel2_scale")
+y_pixel2_scale = st.number_input("Y pixel 2",min_value=0,value=640,on_change=choose_pixel_image_scale,key="y_pixel2_scale")
 distance = st.number_input("Distance in meters",min_value=0.1,value=1.0,key="distance_meters")
 
+
+# Title for making a bridge
+st.title("Bridge creator")
+
+def choose_pixel_image_bridge():
+    '''Callback function for showing the pixels that are chosen'''
+    pil_image = Image.open(uploaded_file).convert('RGB')
+    open_cv_image = np.array(pil_image)
+    open_cv_image = cv2.circle(open_cv_image, (st.session_state['x_pixel1_bridge'],st.session_state['y_pixel1_bridge']), 5, color=(0, 0, 255), thickness=-1)
+    open_cv_image = cv2.circle(open_cv_image, (st.session_state['x_pixel2_bridge'],st.session_state['y_pixel2_bridge']), 5, color=(0, 0, 255), thickness=-1)
+    cv2.imwrite("filter/select_pixel_bridge.png",open_cv_image)
+    make_bridge("./test/test1.png",(st.session_state['x_pixel1_bridge'],st.session_state['y_pixel1_bridge']),(st.session_state['x_pixel2_bridge'],st.session_state['y_pixel2_bridge']))
+    st.session_state.select_pixel_bridge = True
+
+if st.session_state.select_pixel_bridge:
+    st.image("./filter/select_pixel_bridge.png")
+
+x_pixel1_bridge = st.number_input("X pixel 1",min_value=0,value=1,on_change=choose_pixel_image_bridge,key="x_pixel1_bridge")
+y_pixel1_bridge = st.number_input("Y pixel 1",min_value=0,value=1,on_change=choose_pixel_image_bridge,key="y_pixel1_bridge")
+x_pixel2_bridge = st.number_input("X pixel 2",min_value=0,value=640,on_change=choose_pixel_image_bridge,key="x_pixel2_bridge")
+y_pixel2_bridge = st.number_input("Y pixel 2",min_value=0,value=640,on_change=choose_pixel_image_bridge,key="y_pixel2_bridge")
+
+if st.session_state.select_pixel_bridge:
+    st.image("./filter/connected_bridge.png")
 
 # Title for the second part of the GUI selecting which field you want to use. 
 st.title("Select Field")
@@ -95,26 +124,27 @@ st.title("Select Field")
 # When value is changed of the number inputs below run this function
 def select_field():
     '''Callback function for selecting which field to use'''
+    # Initialize distance so that it is always >1
     distance_pixels = 1
-    distance_pixels = math.sqrt((st.session_state['x_pixel2'] - st.session_state['x_pixel1']) ** 2 + (st.session_state['y_pixel2'] - st.session_state['y_pixel1']) ** 2)
-    #print("distance pixels: ",distance_pixels)
+    # Calculate distance
+    distance_pixels = math.sqrt((st.session_state['x_pixel2_scale'] - st.session_state['x_pixel1_scale']) ** 2 + (st.session_state['y_pixel2_scale'] - st.session_state['y_pixel1_scale']) ** 2)
+
+    # Calculate scaling factor
     scale_pixels_per_meter = st.session_state['distance_meters']/distance_pixels
-    #print("Scale pixel: ",scale_pixels_per_meter)
-    predict_json(scale_pixels_per_meter,st.session_state['approx'])
+
+    # Scale the contour
+    predict_json("./filter/connected_bridge.png",scale_pixels_per_meter,st.session_state['approx'])
     filter_json(st.session_state['fieldnr'])
 
     data_path ="./filter/filtered_geojson.json"
 
+    # Plot the field that is selected
     field = load_data(data_path,scale_pixels=1)
     st.session_state["area_field"] = str(field.area.to_numpy()[0])
     fig, ax = plt.subplots()
     field.plot(ax=ax, color='lightblue', edgecolor='black')
     plt.savefig("filter/check_field.png")
 
-    # Uncomment if you want to see pixel plot
-    # fig1, ax1 = plt.subplots()
-    # field_pixel.plot(ax=ax1, color='lightblue', edgecolor='black')
-    # plt.savefig("filter/check_field_pixel.png")
     st.session_state.show_field = True
 
 # Parameters for selecting a field
@@ -130,18 +160,16 @@ if st.session_state.show_field:
 
 # For inputing the parameters of the dynamics of the tractor
 st.title("Parameters path planning")
-turning_radius = st.number_input("Turning radius", value=1)
-distance_AB = st.number_input("Distance between AB lines",value=1)
-headland_size = st.number_input("Headland size",value=1)
+turning_radius = st.number_input("Turning radius", value=1,min_value=1)
+tractor_width = st.number_input("Tractor width",value=1,min_value=1)
+seed_distance = st.number_input("Seed distance",value=1,min_value=1)
 
 # Button to run the path planning algorithm
 def plan_path():
     '''Callback function for path planning button'''
     data_path ="./filter/filtered_geojson.json"
     include_obs = False
-    turning_rad = turning_radius
-    distance = distance_AB
-    field,field_headlands,best_path,sp, swaths_clipped,base, total_path, bases = pathplanning(data_path=data_path,include_obs=include_obs,turning_rad=turning_rad,tractor_width=2,seed_distance=5,plotting=True,interpolation_dist=0.5)
+    field,field_headlands,best_path,sp, swaths_clipped,base, total_path, bases = pathplanning(data_path=data_path,include_obs=include_obs,turning_rad=turning_radius,tractor_width=tractor_width,seed_distance=seed_distance,plotting=True,interpolation_dist=0.5)
     st.session_state['simulate_path'] = best_path
     st.session_state.clicked_path = True
     
@@ -154,7 +182,7 @@ if st.session_state.clicked_path:
 
 # Title for the simulation part with parameters
 st.title("Simulation_result")
-velocity_model = st.number_input("Velocity seeder",value=1.0)
+velocity_model = st.number_input("Velocity",value=1.0)
 iteration_number = st.number_input("Iteration limit",value=500)
 option = st.selectbox("Which controller to use for simulation?",("PID","MPC"))
 
